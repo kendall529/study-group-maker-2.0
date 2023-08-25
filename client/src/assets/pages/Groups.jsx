b6f94d55b879ed9bd5f303a80ca9f3b54523b004
Michaela Brown <michaelarbrown4@gmail.com>
2023-08-24 19:51:32 -0500

    set up the basic groups page

--- client/src/assets/pages/Groups.jsx
+++ client/src/assets/pages/Groups.jsx
@@ -1,10 +1,31 @@
+import { useQuery } from '@apollo/client';
+
+import GroupList from '../components/GroupList';
+
+import { QUERY_GROUPS, QUERY_USERS } from '../utils/queries';
+
 const Groups = () => {
+
+  const { loading, groupData } = useQuery(QUERY_GROUPS);
+  const { loading2, userData } = useQuery(QUERY_USERS);
+
+
+  const groups = groupData?.groups || [];
+  const users = userData?.users || [];
+
   
     return (
       <main>
         <div>
           <div>
-                
+            {loading || loading2 ? (
+              <div>Loading...</div>
+            ) : (
+              <GroupList
+                groups={groups}
+                users={users}
+            />
+          )}
           </div>
         </div>
       </main>
