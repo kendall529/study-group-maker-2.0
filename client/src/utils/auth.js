d510fcfb84629a5c9401e77fe2692af01e0b7868
Michaela Brown <michaelarbrown4@gmail.com>
2023-08-24 20:59:00 -0500

    added jwt auth file

---
+++ client/src/utils/auth.js
@@ -0,0 +1,31 @@
+//decode a token to get user info
+import decode from 'jwt-decode';
+
+// create a new class to instantiate for a user
+class AuthService {
+  // get user data from JSON web token by decoding it
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    return token ? true : false;
+  }
+
+  getToken() {
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken) {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    window.location.reload();
+  }
+}
+
+export default new AuthService();
