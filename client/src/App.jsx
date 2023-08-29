376ac539f0151757747488391b2cb46c05e33997
namhn123 <namhn1231@gmail.com>
2023-08-28 20:07:47 -0500

    changing group models

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,8 @@ import { useState, useEffect } from 'react'
 import { connectWithWebSocket } from './utils/webSockConnection/webSockConnection'
 import React from 'react';
 import {
-  BrowserRouter as Router, Link, useLocation
+  BrowserRouter as Router, Link, useLocation, Outlet
 } from 'react-router-dom';
-
-import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
 
 function App() {
