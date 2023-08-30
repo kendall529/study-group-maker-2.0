b0de6bba10c531e6b700678867bde26a6d9d8cdd
Michaela Brown <michaelarbrown4@gmail.com>
2023-08-29 21:17:53 -0500

    Added dashboard to the list of nav tabs

--- client/src/components/NavTabs.jsx
+++ client/src/components/NavTabs.jsx
@@ -68,6 +68,15 @@ function NavTabs() {
         </li>
         <li className="nav-item block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
           <Link
+            to="/Dashboard"
+            // Check to see if the user profile is the current page
+            className={currentPage === '/Dashboard' ? 'nav-link active' : 'nav-link'}
+          >
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+          <Link
             to="/Contact"
             // Checks to see if the contact page is the current page
             className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
