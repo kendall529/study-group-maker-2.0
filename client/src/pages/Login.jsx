f2741f26db870f5e881260914bb7eb67677e4184
Michaela Brown <michaelarbrown4@gmail.com>
2023-08-30 20:29:27 -0500

    trying to fix login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,7 +8,7 @@ import { registerNewUser } from '../utils/webSockConnection/webSockConnection';
 import Auth from '../utils/auth';
 
 const Login = (props) => {
-    const [formState, setFormState] = useState({ username: '', password:''});
+    const [formState, setFormState] = useState({ name: '', password:''});
     const [login, {error, data}] = useMutation(LOGIN_USER);
 
     
@@ -19,7 +19,7 @@ const Login = (props) => {
             ...formState,
             [name]: value,
         });
-        registerNewUser(username);
+        registerNewUser(name);
     }
 
     // submission form
@@ -39,7 +39,7 @@ const Login = (props) => {
 
     // clearing the form
     setFormState({
-      username: '',
+      name: '',
       password: '',
     });
   };
@@ -56,18 +56,18 @@ const Login = (props) => {
             ) : (
               <form onSubmit={handleFormSubmit}>
                 <div className='mb-6 mt-6 flex justify-center'>
-                  <label for="username" className='block mb-6 text-sm font-medium text-gray-900 dark:text-white'></label>
+                  <label htmlFor="name" className='block mb-6 text-sm font-medium text-gray-900 dark:text-white'></label>
                 <input
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
                   placeholder="Username"
-                  name="username"
-                  type="username"
+                  name="name"
+                  type="name"
                   value={formState.name}
                   onChange={handleChange}
                 />
                 </div>
                 <div className='mb-6 flex justify-center'>
-                  <label for="password" className='block mb-6 text-sm font-medium text-gray-900 dark:text-white'></label>
+                  <label htmlFor="password" className='block mb-6 text-sm font-medium text-gray-900 dark:text-white'></label>
                 <input
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/5 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
                   placeholder="Password"
